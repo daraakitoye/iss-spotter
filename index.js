547da22b668cedce3e4beabcec26a2104cc5b37a
Dara Akitoye <daraakitoye1@gmail.com>
2021-08-23 07:57:28 +0000

    refactored index.js a bit

--- index.js
+++ index.js
@@ -5,9 +5,8 @@ nextISSTimesForMyLocation((error, passTimes) => {
     return console.log("It didn't work!", error);
   }
   //success, print details
-  const results = passTimes
   const dateAndTime = new Date().toString()
-  for (const obj of results) {
+  for (const obj of passTimes) {
     console.log(`Next pass at ${dateAndTime} for ${obj['duration']} seconds.`)
   }
 });
